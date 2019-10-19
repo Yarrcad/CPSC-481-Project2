ae3667039bc78fd711e01fd01521f874ec8903f5
Yarrcad <jonathancardinale@gmail.com>
2019-10-19 13:45:51 -0700

    .

--- GoBotEngine2/kgs_download/train_generator.py
+++ GoBotEngine2/kgs_download/train_generator.py
@@ -1,31 +1,37 @@
 from dlgo.data.parallel_processor import GoDataProcessor
-from dlgo.encoders.oneplane import OnePlaneEncoder
+# from dlgo.encoders.oneplane import OnePlaneEncoder
+from dlgo.encoders.sevenplane import SevenPlaneEncoder
 
-from dlgo.networks import small
+# from dlgo.networks import small
+from dlgo.networks import large
 from keras.models import Sequential
 from keras.layers.core import Dense
 from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adagrad
 
 if __name__ == '__main__':
     go_board_rows, go_board_cols = 19, 19
     num_classes = go_board_rows * go_board_cols
     num_games = 100
 
-    encoder = OnePlaneEncoder((go_board_rows, go_board_cols))
+    # encoder = OnePlaneEncoder((go_board_rows, go_board_cols))
+    encoder = SevenPlaneEncoder((go_board_rows, go_board_cols))
 
     processor = GoDataProcessor(encoder=encoder.name())
 
     generator = processor.load_go_data('train', num_games, use_generator=True)
     test_generator = processor.load_go_data('test', num_games, use_generator=True)
 
-
     input_shape = (encoder.num_planes, go_board_rows, go_board_cols)
-    network_layers = small.layers(input_shape)
+    # network_layers = small.layers(input_shape)
+    network_layers = large.layers(input_shape)
     model = Sequential()
     for layer in network_layers:
         model.add(layer)
     model.add(Dense(num_classes, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    # model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    adagrad = Adagrad()
+    model.compile(loss='categorical_crossentropy', optimizer='adagrad', metrics=['accuracy'])
 
     epochs = 5
     batch_size = 128
@@ -34,7 +40,8 @@ if __name__ == '__main__':
                         steps_per_epoch=generator.get_num_samples() / batch_size,
                         validation_data=test_generator.generate(batch_size, num_classes),
                         validation_steps=test_generator.get_num_samples() / batch_size,
-                        callbacks=[ModelCheckpoint('../checkpoints/small_model_epoch_{epoch}.h5')])
+                        # callbacks=[ModelCheckpoint('../checkpoints/small_model_epoch_{epoch}.h5')])
+                        callbacks=[ModelCheckpoint('../checkpoints/large_model_epoch_{epoch}.h5')])
 
     model.evaluate_generator(generator=test_generator.generate(batch_size, num_classes),
                              steps=test_generator.get_num_samples() / batch_size)
